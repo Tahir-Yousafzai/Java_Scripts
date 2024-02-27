2998ce74bf801803866d71eb62fd127971eddf8f
Tahir khan <tahirwpdeveloper3@gmail.com>
2024-02-27 10:08:11 +0500

    Assigment Done

---
+++ script.js
@@ -0,0 +1,30 @@
+// Assigment 01
+
+// Question 01
+// Question 02
+
+// alert("Hello Tahir khan");
+
+
+
+// Question 03
+
+// alert("Hello Tahir khan. \nKiya hall  hai");
+
+
+
+// Question 04
+
+
+// alert("Hello Tahir khan");
+
+
+// alert("Hello Tahir khan. \nKiya hall  hai");
+
+
+
+
+// Question 05
+
+
+// console.log("Hello Tahir In console")
